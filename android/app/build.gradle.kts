7931a761fad6059ee6f9834a87102653adf67a3f
PrunesLand <panargya@gmail.com>
2025-07-30 11:12:36 +0200

    Refactor: Update Android NDK and Dart dependencies
    This commit updates the Android NDK version and several Dart package dependencies.
    
    Key changes:
    - In `android/app/build.gradle.kts`:
        - Updated `ndkVersion` from `flutter.ndkVersion` to `"27.0.12077973"`.
    - In `pubspec.lock`:
        - Updated `async` from version `"2.12.0"` to `"2.13.0"`.
        - Updated `fake_async` from version `"1.3.2"` to `"1.3.3"`.
        - Updated `leak_tracker` from version `"10.0.8"` to `"10.0.9"`.
        - Updated `vm_service` from version `"14.3.1"` to `"15.0.0"`.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
 android {
     namespace = "com.example.receipt_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
